f639d5324beca4c653585d9923bab5b6ae3c23fd
Khushboo Jayan <khushboojayan@gmail.com>
2023-08-27 17:54:39 +0100

    gradle using journeyAppps

---
+++ TUDSustainabilityApp/app/src/main/java/com/example/tudsustainabilityapp/CreditScore.java
@@ -0,0 +1,14 @@
+package com.example.tudsustainabilityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreditScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_score);
+    }
+}
---
+++ TUDSustainabilityApp/creditScoreCollector/src/main/java/com/example/creditscorecollector/Capture.java
@@ -0,0 +1,13 @@
+package com.example.creditscorecollector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Capture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
